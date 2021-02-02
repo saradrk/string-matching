3978630694c5df0ece43b35780bab4454fabf1b5
Sara Derakhshani <saraderakhshani@icloud.com>
2021-02-02 21:11:44 +0100

    Implement as command line tool

--- main.py
+++ main.py
@@ -2,14 +2,38 @@
 # Programmierung 1: Projekt
 # Test: String-Matching-Tool
 
-
+import click
 from string_matcher import StringMatcher
 
 
-def main(search_pattern, text):
-    SM = StringMatcher(method='finite-state')
-    return SM.match(search_pattern, text)
+@click.command()
+@click.option('-p',
+              '--pattern',
+              help='The pattern you want to match.',
+              required=True
+              )
+@click.option('-t',
+              '--text',
+              help='The text you want to search the pattern for.',
+              required=True
+              )
+@click.option('-m',
+              '--method',
+              default="naive",
+              show_default=True,
+              type=click.Choice(['naive', 'finite-state'],
+                                case_sensitive=False
+                                ),
+              help="The matching method.",
+              )
+def main(pattern, text, method=None):
+    """Pattern matcher"""
+    if method:
+        SM = StringMatcher(method=method)
+    else:
+        SM = StringMatcher()
+    print(SM.match(pattern, text))
 
 
 if __name__ == '__main__':
-    print(main('', 'ab'))
+    main()
