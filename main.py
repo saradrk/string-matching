9dc0cd08a230c6cf0aad6e8cf91f81df0e87c662
Sara Derakhshani <saraderakhshani@icloud.com>
2021-02-26 14:06:39 +0100

    Comment

--- main.py
+++ main.py
@@ -36,12 +36,40 @@ from string_matcher import StringMatcher
               help="The matching method.",
               )
 def main(case_insensitive, pattern, text_input, method=None):
-    """Pattern matcher."""
+    """Console tool for pattern matching.
+
+    Print either all positions where the input pattern begins in an input
+    string or text document or in all found text documents in an input
+    directory by document.
+
+    Args
+    ----
+    case_insensitive : bool
+        Flag that disables case sensitivity.
+    pattern : string
+        Pattern to match to the text input.
+    text_input : string
+        The text itself, a txt document containing the text or a directory
+        that should be scanned for txt documents.
+    method : string (default is None)
+        One of the implemented matching methods 'naive' or 'finite-state'.
+
+    Returns
+    -------
+    None
+    """
+    # Don't match if the pattern is empty
     if len(pattern) > 0:
+        # If method is defined initilize string matcher with method
+        # otherwise use its default
         if method:
             SM = StringMatcher(method=method)
         else:
             SM = StringMatcher()
+        # Check wether text input is directory or file
+        # If directory scan for text files and match them to pattern
+        # If file match it to pattern
+        # Otherwise handle text input as matching text
         if os.path.isdir(text_input):
             for file in os.listdir(text_input):
                 if file.endswith(".txt"):
